7adccfe2839224631cf11340aeb6d2a7c83b2632
holiwars <31833257+holiwars@users.noreply.github.com>
2018-12-14 14:27:24 -0600

    Update settings.py

--- prueba/settings.py
+++ prueba/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=0kwmcrm7^y*j=9nn5=#x+q(wphxc%=!r)xy(h6c67(w_81q%4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', ' . holiwars.PythonAnywhere.com']
 
